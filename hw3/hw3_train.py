c82248dbe8b853cb4b503db63001342f6a1f8e3e
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-03-26 21:56:14 +0800

    upd hw3

--- hw3/hw3_train.py
+++ hw3/hw3_train.py
@@ -1,74 +1,75 @@
-import sys
-import csv
-import pickle
-import numpy as np
-import pandas as pd
+# import sys
+# import csv
+# import pickle
+# import numpy as np
+# import pandas as pd
 
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten
-from keras.layers import Dense, Dropout, Activation
-from keras.layers import ZeroPadding2D, BatchNormalization
-from keras.optimizers import SGD, Adam
-from keras.utils import np_utils, to_categorical
-from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+# from keras.models import Sequential
+# from keras.layers import Conv2D, MaxPooling2D, Flatten
+# from keras.layers import Dense, Dropout, Activation
+# from keras.layers import ZeroPadding2D, BatchNormalization
+# from keras.optimizers import SGD, Adam
+# from keras.utils import np_utils, to_categorical
+# from keras.layers.pooling import MaxPooling2D, AveragePooling2D
 
-def load_train(train_fpath):
-    data = pd.read_csv(train_fpath)
-    Y_train = np.array(data['label'].values, dtype=int)
-    X_train = []
-    for features in data['feature'].values:
-        split_features = [ int(i) for i in features.split(' ') ]
-        matrix_features = np.array(split_features).reshape(48, 48, 1)
-        X_train.append(matrix_features)
-    X_train = np.array(X_train)
-    return X_train, Y_train
+# def load_train(train_fpath):
+#     data = pd.read_csv(train_fpath)
+#     Y_train = np.array(data['label'].values, dtype=int)
+#     X_train = []
+#     for features in data['feature'].values:
+#         split_features = [ int(i) for i in features.split(' ') ]
+#         matrix_features = np.array(split_features).reshape(48, 48, 1)
+#         X_train.append(matrix_features)
+#     X_train = np.array(X_train)
+#     return X_train, Y_train
 
-train_fpath = sys.argv[1]
-model_fpath = sys.argv[2]
-print('# Train : {}'.format(train_fpath))
-print('# Model : {}'.format(model_fpath))
+# train_fpath = sys.argv[1]
+# model_fpath = sys.argv[2]
+# print('# Train : {}'.format(train_fpath))
+# print('# Model : {}'.format(model_fpath))
 
-X_train, Y_train = load_train(train_fpath)
-Y_train = np_utils.to_categorical(Y_train, 7)
+# X_train, Y_train = load_train(train_fpath)
+# Y_train = np_utils.to_categorical(Y_train, 7)
+# print(Y_train.shape)
 
-model = Sequential()
-# CNN
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same', input_shape=(48, 48, 1)))
-model.add(ZeroPadding2D(padding=(2, 2), data_format='channels_last'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+# model = Sequential()
+# # CNN
+# model.add(Conv2D(256, (3, 3), activation='relu', padding='same', input_shape=(48, 48, 1)))
+# model.add(ZeroPadding2D(padding=(2, 2), data_format='channels_last'))
+# model.add(BatchNormalization())
+# model.add(MaxPooling2D(pool_size=(2, 2)))
 
-for i in range(2):
-    model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(ZeroPadding2D(padding=(1, 1), data_format='channels_last'))
-    model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+# for i in range(2):
+#     model.add(Conv2D(128, (3, 3), activation='relu'))
+#     model.add(BatchNormalization())
+#     model.add(ZeroPadding2D(padding=(1, 1), data_format='channels_last'))
+#     model.add(Conv2D(128, (3, 3), activation='relu'))
+#     model.add(BatchNormalization())
+#     model.add(MaxPooling2D(pool_size=(2, 2)))
+#     model.add(Dropout(0.25))
 
 
-# DNN
-model.add(Dense(512, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.25))
-model.add(Dense(512, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.25))
-model.add(Dense(512, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.25))
-model.add(Dense(units=7, activation='softmax'))
+# # DNN
+# model.add(Dense(512, activation='relu'))
+# model.add(BatchNormalization())
+# model.add(Dropout(0.25))
+# model.add(Dense(512, activation='relu'))
+# model.add(BatchNormalization())
+# model.add(Dropout(0.25))
+# model.add(Dense(512, activation='relu'))
+# model.add(BatchNormalization())
+# model.add(Dropout(0.25))
+# model.add(Dense(units=7, activation='softmax'))
 
-model.summary()
+# model.summary()
 
-model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-model.fit(X_train, Y_train, batch_size=100, epochs=50)
-result = model.evaluate(X_train, Y_train)
-print('\nTrain Acc:', result[1])
+# model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+# model.fit(X_train, Y_train, batch_size=100, epochs=50)
+# result = model.evaluate(X_train, Y_train)
+# print('\nTrain Acc:', result[1])
 
-model.save(model_fpath)
-sys.exit()
+# model.save(model_fpath)
+# sys.exit()
 
 ##################################################
 ##################################################
@@ -96,6 +97,7 @@ from keras.callbacks import ModelCheckpoint,EarlyStopping
 from keras.utils import np_utils
 
 #load data from train.csv
+train_data = sys.argv[1]
 mytrain = pd.read_csv(train_data)
 
 train_x = []
@@ -141,7 +143,7 @@ datagen_train = ImageDataGenerator(
 
 # datagen_train.fit(train_x)
 
-print(train_x.shape)
+print(train_y.shape)
 sys.exit()
 
 #CNN
