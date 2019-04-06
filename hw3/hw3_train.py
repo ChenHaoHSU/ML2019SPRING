4de14b094419e33fae9bbe7312bd88a6bfd352fe
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-04-07 01:29:31 +0800

    upd hw3

--- hw3/hw3_train.py
+++ hw3/hw3_train.py
@@ -54,16 +54,18 @@ print('# Setting model...')
 dropout = 0.25
 model = Sequential()
 # CNN
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_last', input_shape=(48, 48, 1)))
-model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', input_shape=(48, 48, 1)))
+model.add(ZeroPadding2D(padding=(2, 2), data_format='channels_last'))
 model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 for i in range(2):
-    model.add(Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_last'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
     model.add(BatchNormalization())
-    model.add(Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_last'))
+    model.add(ZeroPadding2D(padding=(1, 1), data_format='channels_last'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
     model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(dropout))
 
 model.add(Flatten())
