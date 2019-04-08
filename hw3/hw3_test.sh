dd4b65110d145686a1292e0dd884ef732f81e7ee
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-04-09 02:33:29 +0800

    upd hw3 wget

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
 python3 hw3_test.py $1 $2 model/model.pickle
+
+# best_model=kaggle71217.pickle
+# wget https://www.dropbox.com/s/d828oyg5xip1wro/kaggle71217.pickle?dl=0 -O $best_model
+# python3 hw3_test.py $1 $2 $best_model
