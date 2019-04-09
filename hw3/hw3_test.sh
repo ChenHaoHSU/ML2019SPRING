c1f4f2b05fc5bc80e373b16f6f32fb081e3ee495
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-04-09 19:56:05 +0800

    upd hw3 hdf5

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 hw3_test.py $1 $2 model/model.pickle
+python3 hw3_test.py $1 $2 model/model.hdf5
 
-# best_model=kaggle71217.pickle
+# best_model=kaggle71217.hdf5
 # wget https://www.dropbox.com/s/d828oyg5xip1wro/kaggle71217.pickle?dl=0 -O $best_model
 # python3 hw3_test.py $1 $2 $best_model
--- hw3/hw3_train.sh
+++ hw3/hw3_train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p model
-python3 hw3_train.py $1 model/model.pickle
+python3 hw3_train.py $1 model/model.hdf5
