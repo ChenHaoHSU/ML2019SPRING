9cd7a0d5d2baeef94acc10168ca81c4f8d3e51d7
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-04-12 11:33:56 +0800

    upd hw4 final all 123

--- hw4/hw4.sh
+++ hw4/hw4.sh
@@ -3,11 +3,8 @@
 model=kaggle71217.hdf5
 wget https://www.dropbox.com/s/d828oyg5xip1wro/kaggle71217.pickle?dl=0 -O $model
 
-model=kaggle71217.hdf5
-
 python3 hw4_all.py $1 $2 $model
+python3 hw4_lime.py $1 $2 $model
 # python3 hw4_saliency.py $1 $2 $model
 # python3 hw4_filter.py $1 $2 $model
-# python3 hw4_lime.py $1 $2 $model
 # python3 hw4_extra.py $1 $2 $model
-
--- hw4/hw4_lime.py
+++ hw4/hw4_lime.py
@@ -9,6 +9,7 @@ import keras.backend as K
 from lime import lime_image
 from skimage.segmentation import slic
 
+print('### Lime...')
 # Handle argv
 train_fpath = sys.argv[1]
 output_fpath = sys.argv[2]
@@ -18,8 +19,8 @@ print('# Training data : {}'.format(train_fpath))
 print('# Output path   : {}'.format(output_fpath))
 print('# Model         : {}'.format(model_fpath))
 
-label = [1, 2, 3, 4, 5, 6]
-image_ids = [299, 9, 25, 70, 81, 94]
+label = [0, 1, 2, 3, 4, 5, 6]
+image_ids = [15, 299, 9, 25, 70, 81, 94]
 
 # Load data
 def load_train(train_fpath):
@@ -28,13 +29,11 @@ def load_train(train_fpath):
     Y_label = np.array(data['label'].values, dtype=int)
     X_feature = data['feature'].values
     X_train, Y_train = [], []
-    print(Y_label)
     for l, id in enumerate(image_ids):
         features = X_feature[id]
         Y_train.append(label[l])
         split_features = [ [int(i),int(i),int(i)] for i in features.split(' ') ]
         matrix_features = np.array(split_features).reshape(48, 48, 3)
-        # np.append(matrix_features,np.zeros(48, 48))
         X_train.append(matrix_features)
     if normalization == True:
         X_train = np.array(X_train, dtype=float) / 255.0
@@ -43,29 +42,9 @@ def load_train(train_fpath):
     Y_train = np.array(Y_train)
     return X_train, Y_train
 
-# def load_train(train_fpath):
-#     normalization = False
-#     data = pd.read_csv(train_fpath)
-#     Y_train = np.array(data['label'].values, dtype=int)
-#     X_train = []
-#     for features in data['feature'].values:
-#         split_features = [ int(i) for i in features.split(' ') ]
-#         matrix_features = np.array(split_features).reshape(48, 48, 1)
-#         #matrix_features = np.array(split_features).reshape(48*48)
-#         X_train.append(matrix_features)
-#         break
-#     if normalization == True:
-#         X_train = np.array(X_train, dtype=float) / 255.0
-#     else:
-#         X_train = np.array(X_train, dtype=float)
-#     return X_train, Y_train
-
 print('# Loading data...')
 X_train, Y_train = load_train(train_fpath)
 Y_train = np_utils.to_categorical(Y_train, 7)
-print(X_train.shape)
-# print(X_train)
-print(Y_train)
 
 # Load model
 print('# Loading model...')
@@ -73,48 +52,21 @@ model = load_model(model_fpath)
 
 x_train_rgb = X_train
 
-# from skimage.segmentation import slic, mark_boundaries
-# img=x_train_rgb[0]
-# plt.imshow(mark_boundaries(img, slic(img)))
-# plt.show()
+def predict(input):
+    gray = np.expand_dims(input[:,:,:,0], 3)
+    return model.predict(gray)
 
-# from skimage.segmentation import slic, mark_boundaries
-# def predict(input):
-#     gray = np.array(input[:,:,:,0], ndmin=4)
-#     gray.resize(input.shape[0],48,48,1)
-#     return model.predict(gray)
+def segmentation(input):
+    return slic(input, n_segments=200, compactness=100)
 
-# def segmentation(input):
-#     return slic(input)
-
-# for i, idx in enumerate(image_ids):
-#     explainer = lime_image.LimeImageExplainer()
-#     explaination = explainer.explain_instance(
-#                                     image=x_train_rgb[i],
-#                                     classifier_fn=predict,
-#                                     segmentation_fn=segmentation
-#                                 )
-#     image, mask = explaination.get_image_and_mask(
-#                                     label=label[i],
-#                                     positive_only=True,
-#                                     hide_rest=True,
-#                                     num_features=7,
-#                                     min_weight=0.0
-#                                 )
-#     print(type(image))
-#     print(image)
-#     print(image.shape)
-#     image.astype(int)
-#     plt.imshow(image)
-#     plt.show()
-#     plt.imshow(mask)
-#     plt.show()
-#     plt.imshow(mark_boundaries(image / 2 + 0.5, mask))
-#     plt.show()
-#     # plt.imsave('{}/fig3_{}.jpg'.format(output_fpath, i), image)
-#     print('*** Save image {}/fig3_{}.jpg!'.format(output_fpath, i))
-from keras_explain.lime_ribeiro import Lime
-from keras_explain.prediction_diff import PredictionDiff
-image = X_train[0]
-explainer = PredictionDiff(model)
-exp_pos, exp_neg = explainer.explain(image, 0)
+from skimage.segmentation import mark_boundaries
+for i, idx in enumerate(image_ids):
+    explainer = lime_image.LimeImageExplainer()
+    explanation = explainer.explain_instance(x_train_rgb[i], predict, segmentation_fn=segmentation, num_samples=1000)
+    image, mask = explanation.get_image_and_mask(label[i], positive_only=False, num_features=7, hide_rest=False)
+    image=image.astype(np.uint8)
+    plt.imshow(image)
+    plt.imsave('{}/fig3_{}.jpg'.format(output_fpath, i), image)
+    # plt.show()
+    plt.close()
+    print('*** Save image {}/fig3_{}.jpg!'.format(output_fpath, i))
