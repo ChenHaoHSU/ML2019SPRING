c7a0e522e5ac7a4f21d40848553e5af13a963a8e
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-05-02 18:06:07 +0800

    upd hw6 test

--- hw6/hw6_test.py
+++ hw6/hw6_test.py
@@ -44,7 +44,7 @@ def word_to_vector(X_segment):
     embed = Word2Vec.load(w2v_fpath)
     X_train = np.zeros((len(X_segment), MAX_LENGTH, EMBEDDING_DIM))
     for i in range(len(X_segment)):
-        print('\r#   - Converting texts to vectors ({} / {})'.format(i+1, len(X_segment)), end='', flush=True)
+        print('\r# [Info] Converting texts to vectors... {} / {}'.format(i+1, len(X_segment)), end='', flush=True)
         for j in range(min(len(X_segment[i]), MAX_LENGTH)):
             try:
                 vector = embed[X_segment[i][j]]
@@ -63,12 +63,27 @@ X_segment = text_segmentation(X_test)
 X_test = word_to_vector(X_segment)
 
 ''' Prediction and Output '''
+# print('# [Info] Loading model...')
+# model = load_model(model_fpath)
+# prediction = model.predict(X_test)
+# print('# [Info] Output \'{}\'...'.format(output_fpath))
+# with open(output_fpath, 'w') as f:
+#     f.write('id,label\n')
+#     for i, v in enumerate(prediction):
+#         f.write('%d,%d\n' %(i, np.argmax(v)))
+# print('Done!')
+
 print('# [Info] Loading model...')
-model = load_model(model_fpath)
-prediction = model.predict(X_test)
+total = np.zeros((len(X_test, 2)))
+model_names = ['kaggle74460.h5', 'kaggle74990.h5', 'kaggle73920.h5', 'kaggle74880.h5', 'kaggle75320.h5']
+for i, model_name in enumerate(model_names):
+    model = load_model(model_name)
+    prediction = model.predict(X_test)
+    for j, v in enumerate(prediction):
+        total[j][np.argmax(v)] += 1
 print('# [Info] Output \'{}\'...'.format(output_fpath))
 with open(output_fpath, 'w') as f:
     f.write('id,label\n')
-    for i, v in enumerate(prediction):
+    for i, v in enumerate(total):
         f.write('%d,%d\n' %(i, np.argmax(v)))
 print('Done!')
