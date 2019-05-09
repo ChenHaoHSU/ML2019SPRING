3c0f1f7cff9f1273da9ce44bc5514146195d60bd
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-05-09 20:21:16 +0800

    upd hw6

--- hw6/hw6_w2v.py
+++ hw6/hw6_w2v.py
@@ -79,5 +79,3 @@ Y_train = Y_train[0:119018]
 X_train_segment = text_segmentation(X_train)
 X_test_segment = text_segmentation(X_test)
 embed = build_embed(X_train_segment + X_test_segment)
-X_train = word_to_vector(embed, X_train_segment)
-Y_train = np_utils.to_categorical(Y_train, 2)
