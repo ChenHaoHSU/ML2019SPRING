6b7ea40f201709842da5a6cb2c4f180f0ce9c700
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-06-06 02:33:48 +0800

    hw8

--- hw8/hw8_test.py
+++ hw8/hw8_test.py
@@ -11,38 +11,30 @@ from trainer import trainer
 from model import *
 import torchvision.transforms as transforms
 
-batch_size = 128
-num_transform = 20
-num_emsemble = 1
+batch_size = 256
+
+np.random.seed(0)
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
 test_transform = transforms.Compose([
         transforms.ToPILImage(),
-        transforms.Resize(54),
-        transforms.RandomCrop(48),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(15),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5],std=[0.5]),
             ])
 
 def main():
     predict_list = []
-    for count in range(num_transform):
-        print("ensembling transform"+str(count))
 
-        if count == 0:
-            test_dataset = MyDataset(testx_file=sys.argv[1], 
+    test_dataset = MyDataset(testx_file=sys.argv[1], 
                                     is_train=False, save=True, transform=test_transform)
-        else:
-            test_dataset = MyDataset(loadfiles= "test_x.npy" , 
-                                    is_train=False , save=False, transform=test_transform)            
-        test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False) 
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False) 
         
-        predict_list.append(ensemble(modelname=sys.argv[2], test_loader=test_loader))
-        print(predict_list[-1].shape)
+    predict_list.append(ensemble(modelname=sys.argv[2], test_loader=test_loader))
 
     predict_list = np.array(predict_list)
-    print(predict_list.shape)
     predict_list = predict_list.mean(axis=0)
     predicts = predict_list.argmax(axis=1)
     writepredict(predict=predicts, output=sys.argv[3])
