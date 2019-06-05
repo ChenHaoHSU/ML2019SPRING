0dad2af400962e41c8fdbd73128546d17c73533c
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-06-06 04:11:50 +0800

    upd hw8 quantinize

--- hw8/quantize.py
+++ hw8/quantize.py
@@ -2,7 +2,7 @@ import sys
 import torch
 original_path = sys.argv[1]
 quantized_path = sys.argv[2]
-model_dict = torch.load(original_path)
-for key, value in model_dict.items():
-    model_dict[key] = value.type(torch.cuda.HalfTensor)
-torch.save(model_dict, quantized_path)
+state_dict = torch.load(original_path)
+for k, v in state_dict.items():
+    state_dict[k] = v.type(torch.cuda.HalfTensor)
+torch.save(state_dict, quantized_path)
