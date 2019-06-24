1e1c13683d512a5c1396b5d8cae93f03f6943c48
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-06-25 03:20:41 +0800

    upd final reproduce

--- final/reproduce.sh
+++ final/reproduce.sh
@@ -4,5 +4,5 @@ mkdir -p models
 python3 src/reproduce_setup.py
 
 best_model=resnet50_csv_23.h5
-wget https://www.dropbox.com/s/c0f96jtrq8u8p0z/resnet50_csv_23.h5?dl=0 -O $best_model
+wget https://www.dropbox.com/s/c0f96jtrq8u8p0z/resnet50_csv_23.h5?dl=0 -O models/$best_model
 bash ./test.sh $1
