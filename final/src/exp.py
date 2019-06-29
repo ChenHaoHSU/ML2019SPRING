76758ead490368a3a4760f032e3cd44aef2a2b62
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2019-06-30 02:47:33 +0800

    upd final report

--- final/src/exp.py
+++ final/src/exp.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-history_files = {'Resnet50': 'models/2019-06-22-0655_resnet50.csv',
-                 'Resnet101': 'models/2019-06-22-2331_resnet101.csv',
-                 'Resnet121': 'models/2019-06-24-2146_resnet152.csv'}
+history_files = {'Resnet50': '../models/2019-06-22-0655_resnet50.csv',
+                 'Resnet101': '../models/2019-06-22-2331_resnet101.csv',
+                 'Resnet152': '../models/2019-06-24-2146_resnet152.csv'}
 
 for backbone, history_file in history_files.items():
     history_data = pd.read_csv(history_file)
